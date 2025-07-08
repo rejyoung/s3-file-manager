0fbd986b9452c2783eb69c6f3310f6f6466a0d9c
Ryan Young <youngre511@gmail.com>
2025-07-08 17:55:48 -0400

    feat: add donwloadToDisk method along with support methods

--- src/core/downloadManager.ts
+++ src/core/downloadManager.ts
@@ -1,15 +1,42 @@
-import { GetObjectCommand, HeadObjectCommand } from "@aws-sdk/client-s3";
+import {
+    GetObjectCommand,
+    HeadObjectCommand,
+    HeadObjectCommandOutput,
+} from "@aws-sdk/client-s3";
 import { S3FMContext } from "./context.js";
 import { Readable } from "stream";
 import { backoffDelay, wait } from "../utils/wait.js";
-import { FileStreamOptions, LoadFileOptions } from "../types/input-types.js";
+import {
+    DownloadAllOptions,
+    DownloadLinkOptions,
+    DownloadToDiskOptions,
+    FileStreamOptions,
+    LoadFileOptions,
+} from "../types/input-types.js";
 import { fileTypeFromBuffer } from "file-type";
 import isUtf8 from "is-utf8";
+import { createWriteStream } from "fs";
+import { pipeline } from "stream/promises";
+import { writeFile } from "fs/promises";
+import path from "path";
+import {
+    FileContentType,
+    FileMetadata,
+    GetFileFormatInput,
+} from "../types/internal-types.js";
+import { extension } from "mime-types";
 
 const TEXT_MIME_PREFIXES = ["text/", "application/xml"];
 const TEXT_EXTENSIONS = ["txt", "csv", "xml", "md", "html"];
 
-export class downloadManager {
+/**
+ ╔════════════════════════════════════════════════════════════════════════════════╗
+ ║ 📥 DOWNLOAD MANAGER                                                            ║
+ ║ Manages downloads from S3, supporting buffered and streamed file retrieval,    ║
+ ║ with support for metadata extraction and type detection.                       ║
+ ╚════════════════════════════════════════════════════════════════════════════════╝
+ */
+export class DownloadManager {
     private ctx: S3FMContext;
     constructor(context: S3FMContext) {
         this.ctx = context;
@@ -103,7 +130,7 @@ export class downloadManager {
         const { spanOptions = {} } = options;
 
         const {
-            name: spanName = "S3FileManager.downloadFile",
+            name: spanName = "S3FileManager.loadFile",
             attributes: spanAttributes = {
                 bucket: this.ctx.bucketName,
                 filePath: filePath,
@@ -118,48 +145,18 @@ export class downloadManager {
                 while (true) {
                     try {
                         attempt++;
-                        const fileBuffer: Buffer = await this.ctx.withSpan(
-                            spanName,
-                            spanAttributes,
-                            async () => {
-                                const stream = await this.streamFile(filePath);
-                                return await this.ctx.streamToBuffer(
-                                    stream,
-                                    "Readable"
-                                );
-                            }
+
+                        const stream: Readable = await this.streamFile(
+                            filePath
                         );
+                        const fileBuffer: Buffer =
+                            await this.ctx.streamToBuffer(stream, "Readable");
 
-                        let returnType = "buffer";
-                        const mimeType = await this.getMimeType(filePath);
-                        const fileType = await fileTypeFromBuffer(fileBuffer);
-
-                        if (
-                            mimeType &&
-                            mimeType !== "application/octet-stream"
-                        ) {
-                            if (mimeType === "application/json") {
-                                returnType = "json";
-                            } else if (
-                                TEXT_MIME_PREFIXES.some((prefix) =>
-                                    mimeType.startsWith(prefix)
-                                )
-                            ) {
-                                returnType = "text";
-                            }
-                        } else if (fileType || !isUtf8(fileBuffer)) {
-                            returnType = "buffer";
-                        } else if (filePath.toLowerCase().endsWith("json")) {
-                            returnType = "json";
-                        } else if (
-                            TEXT_EXTENSIONS.some((extension) =>
-                                filePath.toLowerCase().endsWith(extension)
-                            )
-                        ) {
-                            returnType = "text";
-                        } else {
-                            returnType = "buffer";
-                        }
+                        const fileFormat = await this.getFileFormat({
+                            filePath,
+                            callerName: "S3FileManager.loadFile",
+                        });
+                        const returnType = fileFormat.fileType;
 
                         switch (returnType) {
                             case "text":
@@ -172,7 +169,7 @@ export class downloadManager {
                     } catch (error) {
                         this.ctx.handleRetryErrorLogging(
                             attempt,
-                            `to download file: ${filePath}`,
+                            `to load file: ${filePath}`,
                             error
                         );
 
@@ -184,12 +181,200 @@ export class downloadManager {
         return result;
     }
 
-    private async getMimeType(filePath: string): Promise<string | undefined> {
+    // ════════════════════════════════════════════════════════════════
+    // 💾 DOWNLOAD TO DISK
+    // Downloads a file from S3 and writes it to the local file system
+    // ════════════════════════════════════════════════════════════════
+    public async downloadToDisk(
+        filePath: string,
+        outDir: string,
+        options: DownloadToDiskOptions
+    ): Promise<void> {
+        const { spanOptions = {}, outputFilename } = options;
+
+        const {
+            name: spanName = "S3FileManager.downloadToDisk",
+            attributes: spanAttributes = {
+                bucket: this.ctx.bucketName,
+                filePath: filePath,
+                outDir: outDir,
+            },
+        } = spanOptions;
+
+        // Normalize and correctly format outDir
+        const normalizedOutDir = path.normalize(outDir);
+        const formattedOutDir = path.join(normalizedOutDir, "");
+
+        await this.ctx.withSpan(spanName, spanAttributes, async () => {
+            const fileMetadata = await this.getFileMetadata(
+                filePath,
+                "S3FileManager.downloadToDisk"
+            );
+            const originalFileName = path.parse(filePath).name;
+
+            const stream: Readable = await this.streamFile(filePath);
+
+            let fileBuffer: Buffer | undefined;
+            if (
+                fileMetadata.contentLength &&
+                fileMetadata.contentLength <= 200 * 1024 * 1024
+            ) {
+                fileBuffer = await this.ctx.streamToBuffer(stream, "Readable");
+            }
+
+            let destinationPath: string;
+            if (outputFilename) {
+                destinationPath = formattedOutDir + outputFilename;
+            } else {
+                const fileFormat = await this.getFileFormat({
+                    filePath,
+                    callerName: "S3FileManager.downloadToDisk",
+                    mimeType: fileMetadata.mimeType,
+                    fileBuffer,
+                });
+                destinationPath = formattedOutDir + originalFileName;
+
+                if (fileFormat.extension) {
+                    destinationPath += "." + fileFormat.extension;
+                }
+            }
+
+            if (fileBuffer) {
+                await writeFile(destinationPath, fileBuffer);
+            } else {
+                await pipeline(stream, createWriteStream(destinationPath));
+            }
+        });
+    }
+
+    // ════════════════════════════════════════════════════════════════
+    //
+    //
+    // ════════════════════════════════════════════════════════════════
+
+    public downloadAllToDisk(prefix: string, options: DownloadAllOptions) {}
+
+    // ════════════════════════════════════════════════════════════════
+    //
+    //
+    // ════════════════════════════════════════════════════════════════
+    public generateTempDownloadLink(
+        filePath: string,
+        options: DownloadLinkOptions
+    ) {}
+
+    // ════════════════════════════════════════════════════════════════
+    // 🧾 GET FILE METADATA
+    // Retrieves file metadata such as MIME type and content length
+    // ════════════════════════════════════════════════════════════════
+    private async getFileMetadata(
+        filePath: string,
+        callerName: string
+    ): Promise<FileMetadata> {
         const command = new HeadObjectCommand({
             Bucket: this.ctx.bucketName,
             Key: filePath,
         });
-        const response = await this.ctx.s3.send(command);
-        return response.ContentType;
+        const s3MetaData = await this.ctx.withSpan<HeadObjectCommandOutput>(
+            `${callerName} > getMimeType`,
+            { filePath },
+            async () => {
+                let attempt = 0;
+                while (true) {
+                    try {
+                        const response = await this.ctx.s3.send(command);
+
+                        return response;
+                    } catch (error: any) {
+                        if (
+                            error.name === "NotFound" ||
+                            error.$metadata?.httpStatusCode === 404
+                        ) {
+                            throw new Error(`File ${filePath} not found`);
+                        }
+                        this.ctx.handleRetryErrorLogging(
+                            attempt,
+                            `to get MIME type of file ${filePath}`,
+                            error
+                        );
+                    }
+                }
+            }
+        );
+
+        if (!s3MetaData.ContentType)
+            this.ctx.logger.warn(`Missing ContentType for ${filePath}`);
+
+        const fileMetadata = {
+            mimeType: s3MetaData.ContentType,
+            contentLength: s3MetaData.ContentLength,
+        };
+
+        return fileMetadata;
+    }
+
+    // ════════════════════════════════════════════════════════════════
+    // 🧪 DETERMINE FILE FORMAT
+    // Determines file content type and best-guess extension
+    // ════════════════════════════════════════════════════════════════
+    private async getFileFormat({
+        filePath,
+        callerName,
+        fileBuffer,
+        mimeType,
+    }: GetFileFormatInput): Promise<{
+        fileType: FileContentType;
+        extension: string;
+    }> {
+        const filePathLC = filePath.toLowerCase();
+
+        if (!mimeType) {
+            mimeType = (
+                await this.getFileMetadata(
+                    filePath,
+                    `${callerName} > getFileFormat`
+                )
+            ).mimeType;
+        }
+
+        const fileType = fileBuffer
+            ? await fileTypeFromBuffer(fileBuffer)
+            : undefined;
+
+        // Determine file content type
+        let returnType: FileContentType = "buffer";
+
+        if (mimeType && mimeType !== "application/octet-stream") {
+            if (mimeType === "application/json") {
+                returnType = "json";
+            } else if (
+                TEXT_MIME_PREFIXES.some((prefix) => mimeType.startsWith(prefix))
+            ) {
+                returnType = "text";
+            }
+        } else if (fileType || (fileBuffer && !isUtf8(fileBuffer))) {
+            returnType = "buffer";
+        } else if (filePathLC.endsWith("json")) {
+            returnType = "json";
+        } else if (
+            TEXT_EXTENSIONS.some((extension) => filePathLC.endsWith(extension))
+        ) {
+            returnType = "text";
+        } else {
+            returnType = "buffer";
+        }
+
+        // Get file extension
+        let ext =
+            extension(mimeType || "") ||
+            fileType?.ext ||
+            path.extname(filePath).slice(1);
+
+        if (ext === "")
+            this.ctx.logger.warn(
+                `Unable to determine a file extension for file ${filePath}`
+            );
+
+        return { fileType: returnType, extension: ext };
     }
 }
